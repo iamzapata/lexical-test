bff918a7a6f706aedd8e0e295aaa372577cc6aa7
Andres Zapata <me@azapata.io>
2022-05-24 21:25:01 +0200

    Setup dev playground

--- src/components/RichTextEditor/appSettings.ts
+++ src/components/RichTextEditor/appSettings.ts
@@ -22,8 +22,7 @@ export type Settings = Record<SettingName, boolean>
 
 const hostName = window.location.hostname
 export const isDevPlayground: boolean =
-  hostName !== 'playground.lexical.dev' &&
-  hostName !== 'lexical-playground.vercel.app'
+  hostName === 'localhost:3000' || hostName === 'lexical-test.vercel.app'
 
 export const DEFAULT_SETTINGS: Settings = {
   disableBeforeInput: false,
