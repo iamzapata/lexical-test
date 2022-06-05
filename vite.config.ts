4feb348ed293ea1859ea3763a473c5f071261540
Andres Zapata <andres@gocleary.com>
2022-06-05 14:06:59 +0200

    Use vite alias resolve for yjs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 /// <reference types="vite/client" />
 // Takem from: https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -9,6 +10,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      yjs: path.resolve('./node_modules/yjs/src/index.js'),
+    },
+  },
   test: {
     update: true,
     globals: true,
